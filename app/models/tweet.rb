7c771e255c5a8c5641d0ef77dad205d9dcd06476
jun06262111-RGB <jun0626.2111@keio.jp>
2026-09-13 19:47:31 +0900

    変更③

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,4 +4,14 @@ class Tweet < ApplicationRecord
     has_many :likes, dependent: :destroy
     has_many :liked_users, through: :likes, source: :user
     has_many :comments, dependent: :destroy
+
+    # 必須項目
+    validates :music_name, presence: true
+    validates :music_artist, presence: true
+    validates :music_year, presence: true
+    validates :music_genre, presence: true
+    validates :favorite_point, presence: true
+    validates :review, presence: true
+    validates :favorite_phrase, presence: true
+    validates :youtube_url, presence: true
 end
